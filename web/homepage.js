e99dfbeaea54660b100cb1889a6040ceab75db17
Jaredbyu <bro15032@byui.edu>
2017-09-23 17:26:02 -0600

    Added homepage

---
+++ web/homepage.js
@@ -0,0 +1,11 @@
+// homepage.js
+
+function bigImg(x) {
+    x.style.height = "350px";
+    x.style.width = "550px";
+}
+
+function normalImg(x) {
+    x.style.height = "250px";
+    x.style.width = "450px";
+}
