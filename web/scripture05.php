bbdea58d5be3f58d2f2e5ff0eac1a798ab7064d6
Jaredbyu <bro15032@byui.edu>
2017-10-11 10:59:23 -0600

    update

--- web/scripture05.php
+++ web/scripture05.php
@@ -1,15 +1,31 @@
 <?php
-try
-{
- $user = 'postgres';
- $password = 'Bigdaddy745azzuron';
- $db = new PDO('pgsql:host=127.0.0.1;dbname=scriptures_db', $user, $password);
-}
-catch (PDOException $ex)
-{
- echo 'Error!: ' . $ex->getMessage();
- die();
-}
+$db = NULL;
+	try {
+		
+		$dbUrl = getenv('DATABASE_URL');
+		if (!isset($dbUrl) || empty($dbUrl)) {
+			
+			$dbUrl = "postgres://ta_user:ta_pass@localhost:5432/scripture_ta";
+			
+		}
+		// Get the various parts of the DB Connection from the URL
+		$dbopts = parse_url($dbUrl);
+		$dbHost = $dbopts["host"];
+		$dbPort = $dbopts["port"];
+		$dbUser = $dbopts["user"];
+		$dbPassword = $dbopts["pass"];
+		$dbName = ltrim($dbopts["path"],'/');
+		// Create the PDO connection
+		$db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+		
+		$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch (PDOException $ex) {
+		// If this were in production, you would not want to echo
+		// the details of the exception.
+		echo "Error connecting to DB. Details: $ex";
+		die();
+	}
 ?>
 
 <!DOCTYPE html>
