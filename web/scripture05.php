e98967c75318630c46295c884786071a96033940
Jaredbyu <bro15032@byui.edu>
2017-10-11 10:53:22 -0600

    update

--- web/scripture05.php
+++ web/scripture05.php
@@ -3,7 +3,7 @@ try
 {
  $user = 'postgres';
  $password = 'Bigdaddy745azzuron';
- $db = new PDO('pgsql:host=127.0.0.1;scriptures_db=myTestDB', $user, $password);
+ $db = new PDO('pgsql:host=127.0.0.1;dbname=scriptures_db', $user, $password);
 }
 catch (PDOException $ex)
 {
@@ -18,7 +18,14 @@ catch (PDOException $ex)
 
 <body>
   <p>
-
+<?php foreach ($db->query('SELECT book, chapter, verse, content FROM scriptures') as $row)
+       {
+           echo '<b>' . $row['book'] . ' ' . $row['chapter'] . ':' . $row['verse']. '</b>';
+           echo ' - "' . $row['content'] . '"';            
+           echo '<br/>';
+       }
+       
+       ?>
   </p>
 </body>
 </html>
