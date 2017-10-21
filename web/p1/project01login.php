1113860a2ef1a42a061f5a85c16b6c884cb5eaa0
Jaredbyu <bro15032@byui.edu>
2017-10-21 14:56:35 -0600

    Update

--- web/p1/project01login.php
+++ web/p1/project01login.php
@@ -11,19 +11,17 @@ $state = $_POST['state'];
 $zip = $_POST['zip'];
 $street = $_POST['street'];
 $password = $_POST['password'];
-$date = date_default_timezone_get();
 $city = $_POST['city'];
 
 try
 {
-	$query = 'INSERT INTO users(first_name, middle_name, last_name, creation_date, user_password, city, state_province, postal_code, street_address) VALUES(:firstName, :middleName, :lastName, :date, :password, :city, :state, :zip, :street)';
+	$query = 'INSERT INTO users(first_name, middle_name, last_name, user_password, city, state_province, postal_code, street_address) VALUES(:firstName, :middleName, :lastName, :password, :city, :state, :zip, :street)';
 	$statement = $db->prepare($query);
 	// Now we bind the values to the placeholders. This does some nice things
 	// including sanitizing the input with regard to sql commands.
 	$statement->bindValue(':firstName', $firstName);
 	$statement->bindValue(':middleName', $middleName);
 	$statement->bindValue(':lastName', $lastName);
-	$statement->bindValue(':date', $date);
     $statement->bindValue(':password', $password);
     $statement->bindValue(':city', $city);
     $statement->bindValue(':zip', $zip);
@@ -41,6 +39,44 @@ catch (Exception $ex)
 	die();
 }
 
-header("project01.php");
-die(); 
+//header("project01.php");
+//die(); 
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Virgil's Pizza</title>
+    <link rel="stylesheet" type="text/css" href="project01.css">
+    
+</head>
+
+<body>
+<div class="headerC">
+
+<h1>Virgil's Pizza</h1>
+    <div class="dropdown">
+  <button class="dropbtn">Menu &darr;</button>
+  <div class="dropdown-content">
+    <a href="project01pizzap.php">Pizza</a>
+    <a href="project01drinksp.php">Drinks</a>
+    <a href="project01dessertp.php">Desserts</a>
+        </div>
+        
+    
+</div>
+    
+    <input type="button" onclick="location.href='project01.php';" class="home" value="Home"/>
+    
+    <hr>
+
+<strong>Thankyou for creating an account with us today</strong>
+ 
+
+<footer class="footerBot">
+        
+        Posted by: Jared Brown
+        <strong> NOTE: This Webpage is A fake Pizza Ordering Service</strong>
+        
+    </footer>
+</body>
+</html>
