98fe4fb9dccecaac0c1fcce94068bdc8fb49dafb
Jaredbyu <bro15032@byui.edu>
2017-10-28 16:32:32 -0600

    update

--- web/p1/project01CompleteOrder.php
+++ web/p1/project01CompleteOrder.php
@@ -24,10 +24,13 @@ echo "after db connect";
 $query = 'INSERT INTO sales(pizzaname, cost, customer, dateofsale) VALUES(:pizzaname, :cost :customer, :dateofsale)';
 echo "after insert into query";
 $statement = $db->prepare($query);
+echo "after prepare query";
 $statement->bindValue(':pizzaname', $pizzaname);
+echo " after bind pizza name ";
 $statement->bindValue(':cost', $cost);
 $statement->bindValue(':customer', $customer);
 $statement->bindValue(':dateofsale', $dateofsale);
+echo "before execute ";
 $statement->execute();
 
 echo "at the end";
