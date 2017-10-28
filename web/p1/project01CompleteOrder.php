01d7427c35a0719be30538682dbe05d542281355
Jaredbyu <bro15032@byui.edu>
2017-10-28 16:13:27 -0600

    added checkout

--- web/p1/project01CompleteOrder.php
+++ web/p1/project01CompleteOrder.php
@@ -16,12 +16,12 @@ $pizzaName = $_POST['Pizza'];
 $cost = 8;
 $dateofsale = date("Y-m-d");
 
-echo $pizzaName. ' ';
+echo $pizzaName. ' '. $cost .' '.$dateofsale.' '.$customer;
 
 require("dbConnect.php");
 $db = get_db();
 
-$query = 'INSERT INTO sales(pizzaName, cost, customer, dateofsale) VALUES(:pizzaName, :cost :username, :dateofsale)';
+$query = 'INSERT INTO sales(pizzaName, cost, customer, dateofsale) VALUES(:pizzaName, :cost :customer, :dateofsale)';
 $statement = $db->prepare($query);
 $statement->bindValue(':customer', $customer);
 $statement->bindValue(':cost', $cost);
