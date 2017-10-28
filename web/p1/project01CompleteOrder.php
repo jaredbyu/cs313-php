127c5d29b828f43fca7f943f59ec59f4dec90fc2
Jaredbyu <bro15032@byui.edu>
2017-10-28 16:41:01 -0600

    update

--- web/p1/project01CompleteOrder.php
+++ web/p1/project01CompleteOrder.php
@@ -13,26 +13,22 @@ else
 
 
 $pizzaname = $_POST['Pizza'];
-$cost = 8.00;
+$cost = 8.25;
 $dateofsale = date("Y-m-d");
 
 echo $pizzaname. ' '. $cost .' '.$dateofsale.' '.$customer. ' ';
 
 require("dbConnect.php");
 $db = get_db();
-echo "after db connect ";
 $query = 'INSERT INTO sales(pizzaname, cost, customer, dateofsale) VALUES(:pizzaname, :cost :customer, :dateofsale)';
-echo "after insert into query ";
 $statement = $db->prepare($query);
-echo "after prepare query";
 $statement->bindValue(':pizzaname', $pizzaname);
-echo " after bind pizza name ";
 $statement->bindValue(':cost', $cost);
 $statement->bindValue(':customer', $customer);
 $statement->bindValue(':dateofsale', $dateofsale);
-echo "before execute ";
 
-if($statement->execute()){
+$result = $statement->execute();
+if($result){
     echo "it worked! <br>";
 }else{
     echo "<br>Execute failed";
